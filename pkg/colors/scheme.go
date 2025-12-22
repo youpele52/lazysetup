3a7db7b6a0fa4ec87f1f10d7f8d1ade1d819be01
Youpele Michael <mjayjesus@gmail.com>
2025-12-23 01:20:55 +0200

    Implement gocui TUI to select and check installers

---
+++ pkg/colors/scheme.go
@@ -0,0 +1,20 @@
+package colors
+
+import "github.com/jesseduffield/gocui"
+
+const (
+	// Background colors
+	BgDark = gocui.ColorDefault
+
+	// Text colors
+	TextPrimary   = gocui.ColorWhite
+	TextSecondary = gocui.ColorCyan
+
+	// Highlight colors
+	HighlightBg = gocui.ColorMagenta
+	HighlightFg = gocui.ColorBlack
+
+	// Accent colors
+	AccentPrimary = gocui.ColorMagenta
+	AccentText    = gocui.ColorCyan
+)
