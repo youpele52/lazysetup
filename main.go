71ace9191a88e5ccb45d62c3e3a753cfc32dbeb2
Youpele Michael <mjayjesus@gmail.com>
2025-12-23 03:55:24 +0200

    Refactor UI to implement multi-panel layout with installation, tools, and progress views
    Extracted keybinding setup into separate function, added multi-panel page with three synchronized panels (installation methods, tools selection, progress/results), implemented panel navigation and switching, added logo display, and updated state model to track active panel. Moved all keybinding logic from main.go to ui package for better organization.

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"time"
 
 	"github.com/jesseduffield/gocui"
-	"github.com/youpele52/lazysetup/pkg/handlers"
 	"github.com/youpele52/lazysetup/pkg/models"
 	"github.com/youpele52/lazysetup/pkg/ui"
 )
@@ -20,54 +19,7 @@ func main() {
 	defer g.Close()
 
 	g.SetLayout(ui.Layout(state))
-
-	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, handlers.Quit); err != nil {
-		log.Panicln(err)
-	}
-
-	if err := g.SetKeybinding("menu", gocui.KeyArrowUp, gocui.ModNone, handlers.CursorUp(state)); err != nil {
-		log.Panicln(err)
-	}
-
-	if err := g.SetKeybinding("menu", gocui.KeyArrowDown, gocui.ModNone, handlers.CursorDown(state)); err != nil {
-		log.Panicln(err)
-	}
-
-	if err := g.SetKeybinding("menu", gocui.KeyEnter, gocui.ModNone, handlers.SelectMethod(state)); err != nil {
-		log.Panicln(err)
-	}
-
-	if err := g.SetKeybinding("menu", gocui.KeyEsc, gocui.ModNone, handlers.GoBack(state)); err != nil {
-		log.Panicln(err)
-	}
-
-	if err := g.SetKeybinding("result", gocui.KeyEsc, gocui.ModNone, handlers.GoBack(state)); err != nil {
-		log.Panicln(err)
-	}
-
-	if err := g.SetKeybinding("tools", gocui.KeyArrowUp, gocui.ModNone, handlers.ToolsCursorUp(state)); err != nil {
-		log.Panicln(err)
-	}
-
-	if err := g.SetKeybinding("tools", gocui.KeyArrowDown, gocui.ModNone, handlers.ToolsCursorDown(state)); err != nil {
-		log.Panicln(err)
-	}
-
-	if err := g.SetKeybinding("tools", gocui.KeySpace, gocui.ModNone, handlers.ToggleTool(state)); err != nil {
-		log.Panicln(err)
-	}
-
-	if err := g.SetKeybinding("tools", gocui.KeyEnter, gocui.ModNone, handlers.StartInstallation(state)); err != nil {
-		log.Panicln(err)
-	}
-
-	if err := g.SetKeybinding("tools", gocui.KeyEsc, gocui.ModNone, handlers.GoBack(state)); err != nil {
-		log.Panicln(err)
-	}
-
-	if err := g.SetKeybinding("results", gocui.KeyEsc, gocui.ModNone, handlers.GoBack(state)); err != nil {
-		log.Panicln(err)
-	}
+	ui.SetupKeybindings(g, state)
 
 	go func() {
 		ticker := time.NewTicker(50 * time.Millisecond)
---
+++ pkg/ui/keybindings.go
@@ -0,0 +1,60 @@
+package ui
+
+import (
+	"log"
+
+	"github.com/jesseduffield/gocui"
+	"github.com/youpele52/lazysetup/pkg/handlers"
+	"github.com/youpele52/lazysetup/pkg/models"
+)
+
+func SetupKeybindings(g *gocui.Gui, state *models.State) {
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, handlers.Quit); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.SetKeybinding("", gocui.KeyTab, gocui.ModNone, handlers.NextPanel(state)); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.SetKeybinding("", '0', gocui.ModNone, handlers.SwitchToPanel(state, models.PanelInstallation)); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.SetKeybinding("", '1', gocui.ModNone, handlers.SwitchToPanel(state, models.PanelTools)); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.SetKeybinding("", '2', gocui.ModNone, handlers.SwitchToPanel(state, models.PanelProgress)); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.SetKeybinding("", gocui.KeyArrowUp, gocui.ModNone, handlers.MultiPanelCursorUp(state)); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.SetKeybinding("", gocui.KeyArrowDown, gocui.ModNone, handlers.MultiPanelCursorDown(state)); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.SetKeybinding("", gocui.KeySpace, gocui.ModNone, handlers.MultiPanelToggleTool(state)); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.SetKeybinding("", gocui.KeyEnter, gocui.ModNone, func(g *gocui.Gui, v *gocui.View) error {
+		if state.CurrentPage == models.PageMultiPanel {
+			if state.ActivePanel == models.PanelInstallation {
+				return handlers.MultiPanelSelectMethod(state)(g, v)
+			} else if state.ActivePanel == models.PanelTools {
+				return handlers.MultiPanelStartInstallation(state)(g, v)
+			}
+		}
+		return nil
+	}); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.SetKeybinding("", gocui.KeyEsc, gocui.ModNone, handlers.GoBack(state)); err != nil {
+		log.Panicln(err)
+	}
+}
